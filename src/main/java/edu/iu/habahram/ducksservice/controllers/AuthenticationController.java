30c52c9bbe57217464e7a58ebf1ac1db4eb785e9
frankbigshuai <yuntzeng@iu.edu>
2024-04-29 15:52:52 -0400

    okok

--- src/main/java/edu/iu/habahram/ducksservice/controllers/AuthenticationController.java
+++ src/main/java/edu/iu/habahram/ducksservice/controllers/AuthenticationController.java
@@ -8,7 +8,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-
+//
 @RestController
 @CrossOrigin
 public class AuthenticationController {
